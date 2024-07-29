10559b456513e84d7d92f3d4359155b45b1cf331
Evilistianingsih <email@anda.com>
2024-07-29 22:55:48 +0700

    latihan sebelum menggunakan database ke 2

--- routes/web.php
+++ routes/web.php
@@ -14,19 +14,70 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
-    return view('about', [
+    return view('About', [
+        "title" => "about",
         "nama" => "Evi Listianingsih",
         "email" => "evilistianingsih10@gmail.com",
         "image" => "10.jpg"
     ]);
 });
 
-
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Evi Listianingsih",
+            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi optio voluptatibus aliquam adipisci et expedita necessitatibus possimus ad reprehenderit illum fuga, rerum est ullam voluptatem nemo at, saepe inventore. Aperiam."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Septi Fitria Munawaroh",
+            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi optio voluptatibus aliquam adipisci et expedita necessitatibus possimus ad reprehenderit illum fuga, rerum est ullam voluptatem nemo at, saepe inventore. Aperiam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe quidem labore sint, ullam officiis omnis ut quas harum? Excepturi quidem aperiam numquam. Expedita ducimus, veritatis sit quisquam nulla sed placeat!"
+        ],
+    
+    ];
+
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
 });
 
+Route::get('posts/{slug}', function($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Evi Listianingsih",
+            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi optio voluptatibus aliquam adipisci et expedita necessitatibus possimus ad reprehenderit illum fuga, rerum est ullam voluptatem nemo at, saepe inventore. Aperiam."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Septi Fitria Munawaroh",
+            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi optio voluptatibus aliquam adipisci et expedita necessitatibus possimus ad reprehenderit illum fuga, rerum est ullam voluptatem nemo at, saepe inventore. Aperiam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe quidem labore sint, ullam officiis omnis ut quas harum? Excepturi quidem aperiam numquam. Expedita ducimus, veritatis sit quisquam nulla sed placeat!"
+        ],
+    
+    ];
+
+    $new_post = [];
+    foreach ($blog_posts as $post){
+        if($post["slug"] === $slug){
+            $new_post = $post;
+        }
+    }
+
+
+    return view('post',[
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
+});
