e2eb62944264683d4f4d09751d3f2717e523bae7
Evilistianingsih <email@anda.com>
2024-08-19 10:48:52 +0700

    modif post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,30 +3,38 @@
 @section('container')
     <div class="container">
         <div class="row justify-content-center mb-5">
-            <div class="col-md8">
+            <div class="col-md-8"> <!-- Perbaikan typo dari 'col-md8' menjadi 'col-md-8' -->
                 <h1 class="mb-3">{{ $post->title }}</h1>
-                <p>By <a href="/posts?author={{ $post->author->username }}"
-                        class="text-decoration-none">{{ $post->author->name }}</a> in <a
-                        href="/posts?category={{ $post->category->slug }}"
-                        class="text-decoration-none">{{ $post->category->name }}</a></p>
+                <p>
+                    By 
+                    @if ($post->author)
+                        <a href="/posts?author={{ $post->author->username }}" class="text-decoration-none">
+                            {{ $post->author->name }}
+                        </a>
+                    @else
+                        Unknown Author
+                    @endif
+                    in 
+                    <a href="/posts?category={{ $post->category->slug }}" class="text-decoration-none">
+                        {{ $post->category->name }}
+                    </a>
+                </p>
 
                 @if ($post->image)
                     <div style="max-height: 350px; overflow:hidden">
-
                         <img src="{{ asset('storage/' . $post->image) }}" alt="{{ $post->category->name }}"
                             class="img-fluid">
                     </div>
                 @else
                     <div style="max-height: 350px; overflow:hidden">
-
                         <img src="https://www.webworxtechnology.com/wp-content/uploads/2018/06/web-development.jpg"
-                            alt="{{ $post->category->name }}" class="img-fluid ">
+                            alt="{{ $post->category->name }}" class="img-fluid">
                     </div>
                 @endif
+
                 <article class="my-3 fs-5">
                     {!! $post->body !!}
                 </article>
-
 
                 <a href="/posts" class="d-block mt-3">Back to Posts</a>
             </div>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,7 +2,6 @@
 @section('container')
     <h1 class="mb-3 text-center">{{ $title }}</h1>
 
-
     <div class="row justify-content-center mb-3">
         <div class="col-md-6">
             <form action="/posts">
@@ -21,17 +20,15 @@
         </div>
     </div>
 
-    @if ($posts[0]->count())
+    @if ($posts->count())
         <div class="card mb-3">
             @if ($posts[0]->image)
                 <div style="max-height: 350px; overflow:hidden">
-
                     <img src="{{ asset('storage/' . $posts[0]->image) }}" alt="{{ $posts[0]->category->name }}"
                         class="img-fluid">
                 </div>
             @else
                 <div style="max-height: 350px; overflow:hidden">
-
                     <img src="https://www.webworxtechnology.com/wp-content/uploads/2018/06/web-development.jpg"
                         alt="{{ $posts[0]->category->name }}" class="img-fluid ">
                 </div>
@@ -43,46 +40,58 @@
 
                 <p>
                     <small class="text-muted">
-                        By <a href="/posts?author={{ $posts[0]->author->username }}"
-                            class="text-decoration-none">{{ $posts[0]->author->name }}</a> in <a
-                            href="/posts?category={{ $posts[0]->category->slug }}"
-                            class="text-decoration-none">{{ $posts[0]->category->name }}</a>{{ $posts[0]->created_at->diffForHumans() }}
+                        By 
+                        @if ($posts[0]->author)
+                            <a href="/posts?author={{ $posts[0]->author->username }}" class="text-decoration-none">
+                                {{ $posts[0]->author->username }}
+                            </a>
+                        @else
+                            Unknown Author
+                        @endif
+                        in 
+                        <a href="/posts?category={{ $posts[0]->category->slug }}" class="text-decoration-none">
+                            {{ $posts[0]->category->name }}
+                        </a> 
+                        {{ $posts[0]->created_at->diffForHumans() }}
                     </small>
                 </p>
 
                 <p class="card-text">{{ $posts[0]->excerpt }}</p>
 
                 <a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-primary">Read more...</a>
-
             </div>
         </div>
-
-
 
         <div class="container">
             <div class="row">
                 @foreach ($posts->skip(1) as $post)
                     <div class="col-md-4 mb-3">
                         <div class="card">
-                            <div class="position-absolute px-3 py-2 " style="background-color: rgba(0,0,0,0.5)"><a
-                                    href="/posts?category={{ $post->category->slug }}"
-                                    class="text-white text-decoration-none">{{ $post->category->name }}</a></div>
+                            <div class="position-absolute px-3 py-2 " style="background-color: rgba(0,0,0,0.5)">
+                                <a href="/posts?category={{ $post->category->slug }}" class="text-white text-decoration-none">
+                                    {{ $post->category->name }}
+                                </a>
+                            </div>
                             @if ($post->image)
-
-                                    <img src="{{ asset('storage/' . $post->image) }}" alt="{{ $post->category->name }}"
-                                        class="img-fluid">
+                                <img src="{{ asset('storage/' . $post->image) }}" alt="{{ $post->category->name }}" class="img-fluid">
                             @else
-
-                                    <img src="https://www.webworxtechnology.com/wp-content/uploads/2018/06/web-development.jpg"
-                                        alt="{{ $post->category->name }}">
+                                <img src="https://www.webworxtechnology.com/wp-content/uploads/2018/06/web-development.jpg"
+                                    alt="{{ $post->category->name }}">
                             @endif
 
                             <div class="card-body">
                                 <h5 class="card-title">{{ $post->title }}</h5>
                                 <p>
                                     <small class="text-muted">
-                                        By <a href="/posts?author={{ $post->author->username }}"
-                                            class="text-decoration-none">{{ $post->author->name }}</a> in
+                                        By 
+                                        @if ($post->author)
+                                            <a href="/posts?author={{ $post->author->username }}" class="text-decoration-none">
+                                                {{ $post->author->username }}
+                                            </a>
+                                        @else
+                                            Unknown Author
+                                        @endif
+                                        in
                                         {{ $post->created_at->diffForHumans() }}
                                     </small>
                                 </p>
@@ -101,5 +110,4 @@
     <div class="d-flex justify-content-center">
         {{ $posts->links() }}
     </div>
-
 @endsection
